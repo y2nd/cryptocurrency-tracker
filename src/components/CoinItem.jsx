448071cacc53b261a61cbbd98a8507ed54027c85
Youssef T <sheese.711@gmail.com>
2022-06-27 13:56:12 +0200

    crypto section styling is done

--- src/components/CoinItem.jsx
+++ src/components/CoinItem.jsx
@@ -4,27 +4,28 @@ import { Sparklines, SparklinesLine } from "react-sparklines";
 
 const CoinItem = ( {coin} ) => {
   return (
-    <tr>
+    <tr className="h-[80px] border-b overflow-hidden">
         <td><AiOutlineStar /></td>
         <td>{coin.market_cap_rank}</td>
         <td>
-        <div className="">
-            <img src={coin.image} alt={coin.id} />
-            <p>{coin.name}</p>
+        <div className="flex items-center mr-3">
+            <img src={coin.image} alt={coin.id} className="w-6 mr-2 rounded-full"/>
+            <p className="hidden sm:table-cell">{coin.name}</p>
         </div>
         </td>
-        <td>{coin.symbol}</td>
-        <td>{coin.current_price}</td>
-        <td>{coin.price_change_percentage_24h}</td>
-        <td>{coin.total_volume}</td>
-        <td>{coin.market_cap}</td>
+        <td>{coin.symbol.toUpperCase()}</td>
+        <td><p className="mr-3">${coin.current_price.toLocaleString()}</p></td>
+        <td>
+            <p className={coin.price_change_percentage_24h < 0 ? "text-red-600" : "text-green-600"}>{coin.price_change_percentage_24h.toFixed(2)}%</p>
+        </td>
+        <td className="w-[180px] hidden md:table-cell">{coin.total_volume.toLocaleString()}</td>
+        <td className="w-[180px] hidden sm:table-cell">{coin.market_cap.toLocaleString()}</td>
         
         <td>
         <Sparklines data={coin.sparkline_in_7d.price} width={100} height={20}>
             <SparklinesLine color="green" style={{ fill: "none" }}/>
         </Sparklines>
         </td>
-        <td>{coin.last_updated}</td>
     </tr>
   )
 }
--- src/components/CoinSearch.jsx
+++ src/components/CoinSearch.jsx
@@ -1,37 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CoinItem from './CoinItem';
 
 const CoinSearch = ({coins}) => {
+
+  const [SearchText, setSearchText] = useState('');
+
+  const handleChange = (event) => {
+    setSearchText(event.target.value);
+  } 
   console.log(coins);
   return (
-    <div>
-      <div className="">
-        <h1>Search Crypto</h1>
+    <div className="rounded-div my-4">
+      <div className="flex flex-col md:flex-row justify-between pt-4 pb-6 text-center md:text-right">
+        <h1 className="text-2xl font-bold my-2">Search Crypto</h1>
         <form action="">
-          <input type="text" placeholder="Search a coin" />
+          <input 
+            type="text" 
+            placeholder="Search a coin" 
+            className="w-full bg-primary border border-input px-4 py-2 rounded-2xl shadow-xl outline-none" 
+            onChange={handleChange}/>
         </form>
       </div>
 
 
-      <table>
+      <table className="w-full text-center ">
         <thead>
-          <tr>
+          <tr className="border-b">
             <th></th>
-            <th>#</th>
-            <th>Coin</th>
+            <th className="px-4">#</th>
+            <th className="text-left">Coin</th>
             <th></th>
             <th>Price</th>
             <th>24h</th>
-            <th>24h Volume</th>
-            <th>Market</th>
+            <th className="hidden md:table-cell">24h Volume</th>
+            <th className="hidden md:table-cell">Market</th>
             <th>Last 7 days</th>
-            <th>Last Updated</th>
           </tr>
         </thead>
         <tbody>
           
           {
-            coins.map( (coin) => (
+            coins.filter( (value) => {
+              if(SearchText === '') {
+                return value;
+              } else if(value.name.toLowerCase().includes(SearchText.toLowerCase())) {
+                return value;
+              }
+            }).map( (coin) => (
                 <CoinItem coin={coin} key={coin.id} />
               )
             )
