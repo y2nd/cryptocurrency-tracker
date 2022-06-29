3a8fb78e9be6e4c019b7f49a901e682de8caa59a
Youssef T <sheese.711@gmail.com>
2022-06-29 12:21:33 +0200

    sign in sign up logic is working

--- src/routes/Account.jsx
+++ src/routes/Account.jsx
@@ -1,18 +1,35 @@
-import React from 'react'
-import SavedCoins from '../components/SavedCoins'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import SavedCoins from '../components/SavedCoins';
+import { UserAuth } from "../context/AuthContext";
 
 function Account() {
+
+  const {user, logOut} = UserAuth();
+  const navigate = useNavigate();
+
+  const handleSignOut = async () => {
+    try {
+      await logOut();
+      navigate("/");
+    } catch(error) {
+      console.log(error.message);
+    }
+  }
+
   return (
     <div className="max-w-[1140px] mx-auto">
       <div className="flex justify-between items-center my-12 py-8 rounded-div">
         <div className="">
           <h1 className="text-2xl font-bold">Account</h1>
           <div className="">
-            <p>Welcome, User</p>
+            <p>Welcome, {user?.email}</p>
           </div>
         </div>
         <div className="">
-          <button className="border px-6 py-2 rounded-2xl shadow-lg hover:shadow-2xl">Sign Out</button>
+          <button
+           onClick={handleSignOut} 
+            className="border px-6 py-2 rounded-2xl shadow-lg hover:shadow-2xl">Sign Out</button>
         </div>
       </div>
       <div className="flex justify-between items-center my-12 py-8 rounded-div">
--- src/routes/SignIn.jsx
+++ src/routes/SignIn.jsx
@@ -1,25 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {AiFillLock, AiOutlineMail} from "react-icons/ai";
 
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
+import {signIn, UserAuth} from "../context/AuthContext";
 
 function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+  const {signIn} = UserAuth();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setError('');
+
+    try {
+      await signIn(email, password);
+      navigate("/account");
+    } catch (error) {
+      setError(error.message);
+      console.log(error.message);
+    }
+  };
+
+
   return (
     <div>
       <div className="max-w-[400px] mx-auto min-h-[600px] px-4 py-20">
         <h1 className="text-2xl font-bold">Sign In</h1>
-        <form action="">
+        <p className="bg-red-400 p-3 my-2 rounded-md text-white">{error.replace("Firebase: ", '')}</p>
+        <form action="" onSubmit={handleSubmit}>
           <div className="my-4">
             <label htmlFor="">Email</label>
             <div className="my-2 w-full relative rounded-2xl shadow-xl">
-              <input className="w-full p-2 bg-primary border border-input rounded-2xl" type="email" />
+              <input
+                onChange={(event) => setEmail(event.target.value)} 
+                className="w-full p-2 bg-primary border border-input rounded-2xl" type="email" />
               <AiOutlineMail className="absolute right-2 top-3 text-gray-400" />
             </div>
           </div>
           <div className="my-4">
             <label htmlFor="">password</label>
             <div className="my-2 w-full relative rounded-2xl shadow-xl">
-              <input className="w-full p-2 bg-primary border border-input rounded-2xl" type="password" />
+              <input
+                onChange={(event) => setPassword(event.target.value)} 
+                className="w-full p-2 bg-primary border border-input rounded-2xl" type="password" />
               <AiFillLock className="absolute right-2 top-3 text-gray-400"/>
             </div>
           </div>
